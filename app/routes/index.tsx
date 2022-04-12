11426e5d04d6f5cfd266751583a99622939c4219
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-04-13 00:55:44 +0900

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,41 @@
+import { useDataRefresh } from "remix-utils";
+import { useEffect } from "react";
+
+export default function Index() {
+  let { refresh } = useDataRefresh();
+  useEffect(() => {
+    let interval = setInterval(refresh, 5000);
+    return () => clearInterval(interval);
+  }, [refresh]);
+
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
